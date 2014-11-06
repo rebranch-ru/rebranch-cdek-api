9bc4f738859b97381571231b0048128703663958
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-11-06 11:07:25 +0500

    remove duplicated method

--- cdek/api.py
+++ cdek/api.py
@@ -85,32 +85,6 @@ class CDEKAPI(object):
         api_response.data = orders.values()
         return api_response
 
-    def make_status_report_request(self, status_report, method_url=u'status_report.php'):
-        """
-        Производит запрос на полчение статуса отправления
-        :param basestring method_url:
-        :param CDEKDeliveryRequest delivery_request:
-        """
-        xml_element = status_report.to_xml_element(tag_name=u'StatusReport')
-        xml_response = self._make_api_request(xml_element, method_url)
-        api_response = Response(status=Response.STATUS_OK, request_element=xml_element)
-        orders = {}
-        for api_order in xml_response.findall('Order'):
-            order_number = api_order.get(u'Number')
-            if api_order.get('ErrorCode'):
-                order = orders.get(order_number)
-                if not order:
-                    order = ResponseOrder(number=order_number)
-                    orders[order_number] = order
-                error = ResponseError(code=api_order.get(u'ErrorCode'), message=api_order.get(u'Msg'), )
-                order.add_error(error)
-                api_response.status = api_response.STATUS_FAIL
-            elif api_order.get(u'DispatchNumber'):
-                order = ResponseOrder(number=order_number, dispatch_number=api_order.get(u'DispatchNumber'))
-                orders[order_number] = order
-        api_response.data = orders.values()
-        return api_response
-
     def _make_api_request(self, xml_element, method_url):
         xml_document = tostring(xml_element, encoding='UTF-8').replace("'", "\"")
         url = urlparse.urljoin(self._api_host, method_url)
